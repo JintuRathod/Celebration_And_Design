937a782181c84cb0b42c07c063d3f7dccbdc6dc7
ketan <jintubhai01062001@gmail.com>
2025-10-01 13:28:56 +0530

    project uploaded

--- CD/settings.py
+++ CD/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pqr6997@gmail.com'
+EMAIL_HOST_USER = 'djangomail2000@gmail.com'
 EMAIL_HOST_PASSWORD = 'ived bgog rvrh eszs'
